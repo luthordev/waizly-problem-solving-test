dc0e8926d7835cd9cafbbd7e7983a9b5a13df8e7
Luthfi Athorique Ginanjar Putra <luthfiathorique@gmail.com>
2024-03-20 13:51:25 +0700

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,17 +1,14 @@
 function formatTime(input) {
     let times = input.replace("PM", "").replace("AM", "").split(":");
-    const hours = parseInt(times[0]);
-  
-    const date = new Date(2023, 1, 1, hours + (input.includes("PM") ? 12 : 0), times[1], times[2]);
-  
-    return date.toLocaleString("en-US", {
-      hour12: false,
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-    });
-  }
-  
+    let hours = parseInt(times[0]);
+
+    if (input.includes("PM"))
+        hours = (hours + 12) % 24;
+
+    const formattedHours = hours.toString().padStart(2, "0");
+
+    return `${formattedHours}:${times[1]}:${times[2]}`;
+}
 
 console.log(formatTime("12:01:00PM"))
 console.log(formatTime("12:01:00AM"))
